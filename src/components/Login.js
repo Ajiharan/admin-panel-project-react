cf36d2f48acab0d88c756e0aad2b2f0e4118d2be
Ajiharan <baskaranajiharan1243@gmail.com>
2021-08-04 23:51:38 +0530

    update changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,40 @@
 import React from "react";
-
+import styled from "styled-components";
 const Login = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <LoginContainer>
+        <Heading>Sign in</Heading>
+        <Form></Form>
+      </LoginContainer>
+    </Container>
+  );
 };
+
+const Form = styled.form``;
+const Heading = styled.h2`
+  text-transform: uppercase;
+  color: gray;
+  display: flex;
+  justify-content: center;
+  margin: 1rem 0;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  width: 100vw;
+  height: 100vh;
+  padding: 1rem;
+`;
+
+const LoginContainer = styled.div`
+  width: 60vw;
+  border-radius: 5px;
+  height: 80vh;
+  background-color: #f7f7f7;
+  box-shadow: rgb(0 0 0 / 20%) 1px 2px 12px 1px;
+`;
 
 export default Login;
