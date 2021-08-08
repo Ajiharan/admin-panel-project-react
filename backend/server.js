6fba47a1a84aa8394f86df689b305c0534bc4a8f
Ajiharan <baskaranajiharan1243@gmail.com>
2021-08-08 10:24:35 +0530

    backend initialize

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import morgan from "morgan";
+const app = express();
+app.use(express.json());
+dotenv.config();
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+
+const limiter = rateLimit({
+  windowMs: 30 * 60 * 1000,
+  max: 70,
+});
+app.use(limiter);
+const PORT = 5000 | process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`port listen in ${PORT}`);
+});
